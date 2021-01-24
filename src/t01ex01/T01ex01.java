a4a6e539a368dfbaeca1e4f513b922aaaa470149
Rodolfo Lazo <rodolfo.lazo@outlook.com>
2021-01-24 17:48:51 +0000

    Commit inicial

---
+++ src/t01ex01/T01ex01.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package t01ex01;
+/**
+ *
+ * @author rodo
+ */
+public class T01ex01 {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello World");
+    }
+    
+}
